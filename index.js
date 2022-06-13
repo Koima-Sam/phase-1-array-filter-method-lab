10ba55cc9282efdad4ef4af1dd88d5d1a60c70a1
Koima-Sam <sam17koima@gmail.com>
2022-06-13 11:19:14 +0300

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 // Code your solution here
 const drivers = [];
-
-function findMatching(string, drivers){
-    return string.filter(
-        result => result.toLowerCase() === drivers.toLowerCase()
+//returns all drivers that match the passed in name
+function findMatching(record,driverName){
+    return record.filter((elements) => elements.toLowerCase() === driverName.toLowerCase()
     )
 };
-function fuzzyMatch(string, drivers){
-    return string.filter(result => result[0] === drivers[0]);
+function fuzzyMatch(record,nameStart){
+    return record.filter((elements) => elements[0] === nameStart[0]);
 };
-function matchName(string, drivers){
-    return string.filter(record => record.name === drivers)
+function matchName(record, driverName){
+    return record.filter((record) => record.name === driverName)
 };
 
 
